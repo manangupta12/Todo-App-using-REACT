3f3b411867ee6a50628eb07d3cbd2aa48453de31
manangupta12 <manangupta852@gmail.com>
2020-05-12 16:12:08 +0530

    Updates

---
+++ precache-manifest.c978125e2654e5cca383c7d7316020e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7413ef6361b15a0267f8d35efc9264e0",
+    "url": "/Todo-App-using-REACT/index.html"
+  },
+  {
+    "revision": "2dbe2392eeb7fbd967e6",
+    "url": "/Todo-App-using-REACT/static/css/2.46a6b073.chunk.css"
+  },
+  {
+    "revision": "7d6bf130c08084b1959f",
+    "url": "/Todo-App-using-REACT/static/css/main.c6fd9c8f.chunk.css"
+  },
+  {
+    "revision": "2dbe2392eeb7fbd967e6",
+    "url": "/Todo-App-using-REACT/static/js/2.ddcfd0eb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Todo-App-using-REACT/static/js/2.ddcfd0eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d6bf130c08084b1959f",
+    "url": "/Todo-App-using-REACT/static/js/main.05f303da.chunk.js"
+  },
+  {
+    "revision": "c252853daa955cf36f4e",
+    "url": "/Todo-App-using-REACT/static/js/runtime-main.a860a0f1.js"
+  }
+]);
